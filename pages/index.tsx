6b1a2fd2284fd65fcec1f582fe4d73478440e859
JaewookJung <stich9208@seerslab.com>
2022-09-24 22:34:20 +0900

    Add: Home 화면 간략 자기소개 애니메이션 추가
    Signed-off-by: JaewookJung <stich9208@seerslab.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,33 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
+import { useState, useEffect, useRef } from "react";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+  const WELCOME_TEXT_LIST = [
+    { lang: "kr", text: "안녕하세요 프론트엔드 개발자 정재욱입니다" },
+    {
+      lang: "en",
+      text: "HELLO, I`M JAEWOOK JUNG, FRONTEND DEVELOPER",
+    },
+  ];
+
+  const callback = useRef(() => {});
+
+  const [textCount, setTextCount] = useState(0);
+
+  const changeTextCount = () =>
+    setTextCount((textCount) => (textCount + 1) % WELCOME_TEXT_LIST.length);
+
+  callback.current = changeTextCount;
+
+  useEffect(() => {
+    const textCountCallback: any = callback.current;
+
+    const interval = setInterval(textCountCallback, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -15,13 +39,13 @@ const Home: NextPage = () => {
           rel="stylesheet"
         />
       </Head>
-      <nav className={styles.nav}>
+      {/* <nav className={styles.nav}>
         <div>LIST</div>
         <div>SEARCH</div>
-      </nav>
+      </nav> */}
       <main className={styles.main}>
         <div className={styles.info}>
-          <div>ABOUT</div>
+          <span>{WELCOME_TEXT_LIST[textCount].text}</span>
         </div>
         <div className={styles.articles}>
           <div>ARTICLES</div>
@@ -39,7 +63,7 @@ const Home: NextPage = () => {
           <div>item1</div>
         </div>
       </main>
-      <footer className={styles.footer}>
+      {/* <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
@@ -50,7 +74,7 @@ const Home: NextPage = () => {
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
-      </footer>
+      </footer> */}
     </div>
   );
 };
