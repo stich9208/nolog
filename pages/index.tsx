3235dd974da489df5ae5c5a44475ad4220f1ddfb
JaewookJung <stich9208@seerslab.com>
2022-09-26 21:55:37 +0900

    Fix: 애니메이션 초 수정 및 주석 추가
    Signed-off-by: JaewookJung <stich9208@seerslab.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,29 +4,26 @@ import { useState, useEffect, useRef } from "react";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+  //constant state
   const WELCOME_TEXT_LIST = [
-    { lang: "kr", text: "안녕하세요 프론트엔드 개발자 정재욱입니다" },
-    {
-      lang: "en",
-      text: "HELLO, I`M JAEWOOK JUNG, FRONTEND DEVELOPER",
-    },
+    "안녕하세요 프론트엔드 개발자 정재욱입니다",
+    "HELLO, I`M JAEWOOK JUNG, FRONTEND DEVELOPER",
   ];
-
   const callback = useRef(() => {});
 
-  const [textCount, setTextCount] = useState(0);
+  //component state
+  const [textIndex, setTextIndex] = useState(0);
 
-  const changeTextCount = () =>
-    setTextCount((textCount) => (textCount + 1) % WELCOME_TEXT_LIST.length);
-
-  callback.current = changeTextCount;
-
+  //useEffect
   useEffect(() => {
-    const textCountCallback: any = callback.current;
-
-    const interval = setInterval(textCountCallback, 5000);
+    const textCountCallback = callback.current;
+    const interval = setInterval(textCountCallback, 7000);
     return () => clearInterval(interval);
   }, []);
+
+  //functions
+  callback.current = () =>
+    setTextIndex((index) => (index + 1) % WELCOME_TEXT_LIST.length);
 
   return (
     <div className={styles.container}>
@@ -45,7 +42,7 @@ const Home: NextPage = () => {
       </nav> */}
       <main className={styles.main}>
         <div className={styles.info}>
-          <span>{WELCOME_TEXT_LIST[textCount].text}</span>
+          <span>{WELCOME_TEXT_LIST[textIndex]}</span>
         </div>
         <div className={styles.articles}>
           <div>ARTICLES</div>
