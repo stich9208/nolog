efda6fde561b07fcb8560bb386f43aa59ee7eafd
JaewookJung <>
2023-02-02 16:49:05 +0900

    Add: @next/font 적용
    Signed-off-by: JaewookJung <>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Do_Hyeon } from "@next/font/google";
+
+const doHyeon = Do_Hyeon({
+  subsets: ["latin"],
+  weight: "400",
+  style: "normal",
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={doHyeon.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
 
 export default MyApp;
