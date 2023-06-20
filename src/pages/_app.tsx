d3bc49571b69f6085718b128b40ea49c5b198c36
JaewookJung <stich9208@seerslab.com>
2023-06-20 23:05:29 +0900

    Fix: font 주석처리
    Signed-off-by: JaewookJung <stich9208@seerslab.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,16 +7,16 @@ import { RecoilRoot } from "recoil";
 import Layout from "../components/common/Layout";
 import Loading from "../components/common/Loading";
 
-const KIMM = localFont({
-  src: [
-    {
-      path: "../../public/font/KIMM_light.ttf",
-      weight: "300",
-      style: "normal",
-    },
-    { path: "../../public/font/KIMM_bold.ttf", weight: "700", style: "normal" },
-  ],
-});
+// const KIMM = localFont({
+//   src: [
+//     {
+//       path: "../../public/font/KIMM_light.ttf",
+//       weight: "300",
+//       style: "normal",
+//     },
+//     { path: "../../public/font/KIMM_bold.ttf", weight: "700", style: "normal" },
+//   ],
+// });
 
 const MyApp = ({ Component, pageProps, ...appProps }: AppProps) => {
   return (
