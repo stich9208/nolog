639d65daed37796ec1aeef3474aee2ea0c8b1e26
JaewookJung <>
2023-08-04 16:22:30 +0900

    Add: Svg 컴포넌트 추가
    Signed-off-by: JaewookJung <>

--- src/components/common/Svg.tsx
+++ src/components/common/Svg.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import { PropsWithChildren, SVGProps } from "react";
 
-const Svg = () => {
-  return <svg>Svg</svg>;
+interface ISvg extends SVGProps<SVGSVGElement> {
+  size?: number;
+  width?: number;
+  height?: number;
+  color?: string;
+  viewBox?: string;
+}
+
+const Svg = ({
+  size,
+  width,
+  height,
+  color,
+  viewBox = "0 0 30 30",
+  children,
+  ...rest
+}: PropsWithChildren<ISvg>) => {
+  return (
+    <svg
+      width={width ?? size ?? 30}
+      height={height ?? size ?? 30}
+      viewBox={viewBox}
+      fill={color ? color : "none"}
+      {...rest}
+    >
+      {children}
+    </svg>
+  );
 };
 
 export default Svg;
